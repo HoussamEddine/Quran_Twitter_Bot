c1762ec49d74dafe148cdf9dcb09409755595480
Houssam-Eddine <chetouanihoussameddine@gmail.com>
2018-02-02 23:03:46 +0100

    adding changes

--- main.js
+++ main.js
@@ -5,14 +5,14 @@ var generateVerse = require('./generateVerse.js');
 
 
 
+var T = new Twit({
+  consumer_key: process.env.consumer_key,
+  consumer_secret: process.env.consumer_secret,
+  access_token: process.env.access_token,
+  access_token_secret: process.env.access_token_secret,
+});
 function sendTweet() {
 
-  var T = new Twit({
-    consumer_key: process.env.consumer_key,
-    consumer_secret: process.env.consumer_secret,
-    access_token: process.env.access_token,
-    access_token_secret: process.env.access_token_secret,
-  });
   var verse = generateVerse();
 
   var verseString = verse.verse + " \n " + verse.verseEn + " \n Name: " + verse.surahName + " \n Verse: " + verse.randomVerseNumber + " \n Index: " + verse.surahIndex + "\n Type: " + verse.surahType;
